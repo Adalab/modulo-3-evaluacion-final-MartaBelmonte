252f1a28fafd8efd83b05315bc09f36ca45339dd
MartaBelmonte <martabelmonts5@gmail.com>
2023-07-02 20:58:13 +0200

    keydown

--- src/components/CharacterDetail/CharacterDetail.js
+++ src/components/CharacterDetail/CharacterDetail.js
@@ -3,18 +3,20 @@ import { useParams, Link, useNavigate, useLocation } from 'react-router-dom';
 import Dead from '../../Images/dead.png';
 
 function CharacterDetail({ characters }) {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const location = useLocation();
+  const { id } = useParams();   // Obtiene el parámetro 'id' de la URL actual
+  const navigate = useNavigate();  // Obtiene la función 'navigate' para navegar a diferentes rutas
+  const location = useLocation();  // Obtiene información sobre la ubicación actual de la aplicación
 
+  // Busca el personaje seleccionado en el array de 'characters'
   useEffect(() => {
     const selectedCharacter = characters.find((character) => character.id === parseInt(id));
-    if (!selectedCharacter) {
+    if (!selectedCharacter) {  // Si no encuentra el personaje, navega a la ruta principal
       navigate('/');
     }
   }, [characters, id, navigate]);
 
   useEffect(() => {
+    // Maneja la navegación hacia la ruta principal cuando se presiona la tecla de flecha izquierda
     const handleBackNavigation = (event) => {
       if (event.persist) {
         event.persist();
@@ -24,18 +26,24 @@ function CharacterDetail({ characters }) {
       }
     };
 
+    // Agrega un event listener para escuchar los eventos 'keydown' (tecla presionada)
     window.addEventListener('keydown', handleBackNavigation);
 
+    // Elimina el event listener anterior cuando no hace eso
     return () => {
       window.removeEventListener('keydown', handleBackNavigation);
     };
   }, [navigate, location]);
 
+  // Busca el objeto de 'selectedCharacter' cuyo 'id' coincide con el 'id' proporcionado en la URL
   const selectedCharacter = characters.find((character) => character.id === parseInt(id));
+
+  // Si no se encontró el personaje seleccionado, no se renderiza nada
   if (!selectedCharacter) {
     return null;
   }
 
+  // Renderiza la información del personaje y los elementos HTML correspondientes
   return (
     <div className="character-detail">
       <h2 className="character-detail-name">{selectedCharacter.name}</h2>
@@ -57,6 +65,7 @@ function CharacterDetail({ characters }) {
 }
 
 export default CharacterDetail;
+
 
 
 
